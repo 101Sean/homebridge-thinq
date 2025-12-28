66b156880b9a39a9bca701195fc3ef7e0f956f25
mikeryu98 <mikeryu98@gmail.com>
2025-12-28 16:31:27 +0900

    v1.0

--- API.js
+++ API.js
@@ -15,8 +15,25 @@ class ThinQConnectAPI {
     }
 
     async fetchDevices() {
-        const res = await this.client.get('/v1/devices');
-        return res.data.result.devices;
+        try {
+            const res = await this.client.get('/v1/devices');
+
+            if (res.data && res.data.result && res.data.result.devices) {
+                return res.data.result.devices;
+            } else if (res.data && res.data.devices) {
+                return res.data.devices;
+            }
+
+            return [];
+        } catch (error) {
+            if (error.response) {
+                console.error('LG API 서버 에러 상태코드:', error.response.status);
+                console.error('LG API 에러 상세 내용:', JSON.stringify(error.response.data, null, 2));
+            } else {
+                console.error('네트워크 또는 기타 에러:', error.message);
+            }
+            throw error;
+        }
     }
 
     async deviceControl(deviceId, propertyId, opMode) {
@@ -28,7 +45,12 @@ class ThinQConnectAPI {
                 "locationName": "MAIN"
             }
         };
-        return await this.client.post(`/v1/devices/${deviceId}/control`, body);
+        try {
+            return await this.client.post(`/v1/devices/${deviceId}/control`, body);
+        } catch (error) {
+            console.error('제어 명령 전송 중 에러:', error.response?.data || error.message);
+            throw error;
+        }
     }
 }
 module.exports = ThinQConnectAPI;
